0494bf228f084c1639bb623cb78879c5d2af5e68
Ann Kolesnicova <ann.kolesnicova@gmail.com>
2020-01-02 17:25:30 +0300

    create intro point

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
+import Header from 'components/Header';
+import About from 'components/About';
+import Main from 'components/Main';
+import BoostBlock from 'components/BoostBlock';
+import Footer from 'components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Header />
+      <About />
+      <Main />
+      <BoostBlock />
+      <Footer />
     </div>
   );
 }
