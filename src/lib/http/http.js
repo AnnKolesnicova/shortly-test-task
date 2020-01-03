03e7ffd2ab987360d03ae994e19632b381849a8b
Ann Kolesnicova <ann.kolesnicova@gmail.com>
2020-01-03 17:35:07 +0300

    replaced headers to service method

--- src/lib/http/http.js
+++ src/lib/http/http.js
@@ -1,16 +1,6 @@
-const key = 'f54646f55c3946e788047a1c7b52a715';
-const workspace = 'bf1a52ade45f4d8988d097e11e476513';
-
-const headers = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
-  'apikey': key,
-  'workspace': workspace,
-};
-
 const request = (url, options) => {
   const requestOptions =
-    Object.assign({}, { headers }, options);
+    Object.assign({}, options);
 
   const { body } = requestOptions;
   requestOptions.body = JSON.stringify(body);
--- src/services/service.js
+++ src/services/service.js
@@ -2,15 +2,28 @@ export const routes = {
   short: `https://api.rebrandly.com/v1/links`,
 };
 
+const key = 'f54646f55c3946e788047a1c7b52a715';
+const workspace = 'bf1a52ade45f4d8988d097e11e476513';
+
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  'apikey': key,
+  'workspace': workspace,
+};
+
 export default class ShortlyService {
   constructor({ http }) {
     this._http = http;
   }
 
   shortenLink({ longUrl }) {
-    return this._http.post(routes.short, {
-      destination: longUrl,
-      domain: { fullName: "rebrand.ly" }
-    });
+    return this._http.post(
+      routes.short, {
+        destination: longUrl,
+        domain: { fullName: 'rebrand.ly' },
+      }, {
+        headers,
+      });
   }
 }
